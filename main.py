1fb9cf64fb3f4b00aab314b4bccb7081633c65b8
Xavier Bester <xavierbester6@gmail.com>
2025-07-25 18:48:10 -0600

    made it work nearly finished

--- main.py
+++ main.py
@@ -4,15 +4,31 @@ import api
 
 def getAPI():
     apiKey = api.getAPIKey()
-    return f"https://api.openweathermap.org/data/2.5/weather?lat=50.04303&lon=-110.679016&appid={apiKey}"
+    return requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat=50.04303&lon=-110.679016&appid={apiKey}")
 
 #call all other methods and printing out our beautiful human readable shit
 def main():
     data = getAPI()
-    print(data)
-    parsed = parseData(data)
+    if data.status_code == 200:
+        parsed = parseData(data)
+        print(f"Sky: {parsed[0]}\nSky Description: {parsed[1]}\nCurrent Temp: {parsed[2]}\nLow Temp of: {parsed[3]}\nHigh Temp of: {parsed[4]}")
+    else:
+        print("some kind of error happened with getting the API data")
 
 def parseData(data):
-    print(data)
-    return data
+    data = data.text
+    data = data.replace("[", '').replace(']', '').replace('{', '').replace('}', '').replace(",", ":")
+    data = data.split(':')
+    looking = ['"main"', '"description"', '"temp"', '"temp_min"', '"temp_max"']
+    lookingPointer = 0
 
+    for i in range(len(data)):
+        if lookingPointer < 5 and data[i] == looking[lookingPointer]:
+            looking[lookingPointer] = data[i + 1] 
+            lookingPointer += 1
+
+    return  looking
+
+def kelvinToCel():
+
+main()
