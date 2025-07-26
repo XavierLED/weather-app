9d1a94ea8bbf84f0be53a6b5a6938cf11af7b184
Xavier Bester <xavierbester6@gmail.com>
2025-07-25 20:48:13 -0600

    made it so that its user specific

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import requests
 import api
 import json
 
-
-def getAPI():
+def getAPI(location):
     apiKey = api.getAPIKey()
-    return requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat=50.04303&lon=-110.679016&appid={apiKey}")
+    return requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={apiKey}")
 
-#call all other methods and printing out our beautiful human readable shit
 def main():
-    data = getAPI()
+    city = input("City name (example: Calgary):")
+    data = getAPI(city)
     if data.status_code == 200:
         parsed = parseData(data)
         parsed[2] = round(kelvinToCel(float(parsed[2])))
@@ -17,8 +16,12 @@ def main():
         parsed[4] = round(kelvinToCel(float(parsed[4])))
 
         print(f"Sky: {parsed[0]}\nSky Description: {parsed[1]}\nCurrent Temp: {parsed[2]}\nLow Temp of: {parsed[3]}\nHigh Temp of: {parsed[4]}")
-    else:
+    elif data.status_code == 401:
         print("some kind of error happened with getting the API data")
+    elif data.status_code == 404:
+        print("entered in invalid city name")
+    else:
+        print("some unknown reason is messing with request")
 
 def parseData(data):
     data = data.text
